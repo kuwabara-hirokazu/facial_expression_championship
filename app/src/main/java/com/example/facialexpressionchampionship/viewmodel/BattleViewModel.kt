661fdb9c7b6c0a417d882873c4f6aa82ca302330
hkuwabara-cq <h.kuwabara@caraquri.com>
2021-08-21 12:41:05 +0900

    テーマをリソースIDで管理

--- app/src/main/java/com/example/facialexpressionchampionship/viewmodel/BattleViewModel.kt
+++ app/src/main/java/com/example/facialexpressionchampionship/viewmodel/BattleViewModel.kt
@@ -2,15 +2,25 @@ package com.example.facialexpressionchampionship.viewmodel
 
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import com.example.facialexpressionchampionship.R
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class BattleViewModel @Inject constructor() : ViewModel() {
 
-    private val themeList = listOf("怒り", "軽蔑", "嫌悪", "恐れ", "幸せ", "通常", "悲しみ", "驚き")
+    private val themeList = listOf(
+        R.string.anger,
+        R.string.contempt,
+        R.string.disgust,
+        R.string.fear,
+        R.string.happiness,
+        R.string.neutral,
+        R.string.sadness,
+        R.string.surprise
+    )
 
-    var battleTheme = ObservableField<String>()
+    var battleTheme = ObservableField<Int>()
 
     fun setupBattleTheme() {
         battleTheme.set(themeList.shuffled()[0])
