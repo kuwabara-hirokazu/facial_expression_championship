bd836d5ee736def504ffcfd66bafcc8575e26778
hkuwabara-cq <h.kuwabara@caraquri.com>
2021-08-24 20:30:20 +0900

    BattleViewModelにRepositoryでなくインターフェースを注入させる

--- app/src/main/java/com/example/facialexpressionchampionship/viewmodel/BattleViewModel.kt
+++ app/src/main/java/com/example/facialexpressionchampionship/viewmodel/BattleViewModel.kt
@@ -2,14 +2,14 @@ package com.example.facialexpressionchampionship.viewmodel
 
 import androidx.databinding.ObservableField
 import com.example.facialexpressionchampionship.Signal
-import com.example.facialexpressionchampionship.data.ThemeRepository
+import com.example.facialexpressionchampionship.data.ThemeSource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 import javax.inject.Inject
 
 @HiltViewModel
 class BattleViewModel @Inject constructor(
-    private val repository: ThemeRepository
+    private val repository: ThemeSource
 ) : BaseViewModel() {
 
     var battleTheme = ObservableField<Int>()
