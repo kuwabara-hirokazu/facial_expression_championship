4566b2d91edd774f38bf68e2d739134070b3847c
hkuwabara-cq <h.kuwabara@caraquri.com>
2021-08-24 21:45:49 +0900

    LocalDataでテーマを持たせるように変更

--- app/src/main/java/com/example/facialexpressionchampionship/data/LocalData.kt
+++ app/src/main/java/com/example/facialexpressionchampionship/data/LocalData.kt
@@ -5,14 +5,26 @@ import javax.inject.Inject
 
 class LocalData @Inject constructor(){
 
-    val themeList = listOf(
-        R.string.anger,
-        R.string.contempt,
-        R.string.disgust,
-        R.string.fear,
-        R.string.happiness,
-        R.string.neutral,
-        R.string.sadness,
-        R.string.surprise
-    )
+    companion object {
+        private val themeList = listOf(
+            R.string.anger,
+            R.string.contempt,
+            R.string.disgust,
+            R.string.fear,
+            R.string.happiness,
+            R.string.neutral,
+            R.string.sadness,
+            R.string.surprise
+        )
+
+        var theme = themeList.shuffled()[0]
+    }
+
+    fun getTheme(): Int {
+        return theme
+    }
+
+    fun changeTheme() {
+        theme = themeList.shuffled()[0]
+    }
 }
