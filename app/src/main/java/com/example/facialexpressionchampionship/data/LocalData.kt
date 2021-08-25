0b3e7b26e0e8df377b2142c9cb0250d1925d4cf1
hkuwabara-cq <h.kuwabara@caraquri.com>
2021-08-26 01:31:02 +0900

    themeをenumで保持させるよう変更

--- app/src/main/java/com/example/facialexpressionchampionship/data/LocalData.kt
+++ app/src/main/java/com/example/facialexpressionchampionship/data/LocalData.kt
@@ -1,30 +1,49 @@
 package com.example.facialexpressionchampionship.data
 
 import com.example.facialexpressionchampionship.R
+import com.example.facialexpressionchampionship.model.Emotion
 import javax.inject.Inject
 
 class LocalData @Inject constructor(){
 
     companion object {
-        private val themeList = listOf(
-            R.string.anger,
-            R.string.contempt,
-            R.string.disgust,
-            R.string.fear,
-            R.string.happiness,
-            R.string.neutral,
-            R.string.sadness,
-            R.string.surprise
-        )
+        var themeType = ThemeType.values().toList().shuffled().first()
+    }
 
-        var theme = themeList.shuffled()[0]
+    private fun getThemeId(): Int {
+        return themeType.id
     }
 
     fun getTheme(): Int {
-        return theme
+        return themeType.theme
     }
 
     fun changeTheme() {
-        theme = themeList.shuffled()[0]
+        themeType = ThemeType.values().toList().shuffled().first()
     }
+
+    fun getThemeScore(emotion: Emotion): String {
+        val emotionList = listOf(
+            emotion.anger,
+            emotion.contempt,
+            emotion.disgust,
+            emotion.fear,
+            emotion.happiness,
+            emotion.neutral,
+            emotion.sadness,
+            emotion.surprise
+        )
+        return emotionList[getThemeId()]
+    }
+}
+
+enum class ThemeType(val id: Int, val theme: Int) {
+    ANGER(0, R.string.anger),
+    CONTEMPT(1, R.string.contempt),
+    DISGUST(2, R.string.disgust),
+    FEAR(3, R.string.fear),
+    HAPPINESS(4, R.string.happiness),
+    NEUTRAL(5, R.string.neutral),
+    SADNESS(6, R.string.sadness),
+    SURPRISE(7, R.string.surprise)
 }
