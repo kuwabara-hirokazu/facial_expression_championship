866b93bcff7b47d128acce5731d8c219a0478d89
hkuwabara-cq <h.kuwabara@caraquri.com>
2021-09-11 14:42:40 +0900

    API-KEYを再生成してpropertyファイルに隠蔽する

--- app/src/main/java/com/example/facialexpressionchampionship/ApiRequest.kt
+++ app/src/main/java/com/example/facialexpressionchampionship/ApiRequest.kt
@@ -9,7 +9,7 @@ import retrofit2.http.POST
 
 interface ApiRequest {
 
-    @Headers("Ocp-Apim-Subscription-Key: 19565daf9eef4b368637d70458462ad7")
+    @Headers("Ocp-Apim-Subscription-Key: ${BuildConfig.FACE_API_KEY}")
     @POST("detect?returnFaceAttributes=emotion")
     fun detectFace(
         @Body binaryData: RequestBody
