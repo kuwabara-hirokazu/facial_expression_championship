6eba344abac0c5d11721da2acf08df7f3686519c
hkuwabara-cq <h.kuwabara@caraquri.com>
2021-09-15 21:18:23 +0900

    hiltの不要な設定を削除

--- app/src/main/java/com/example/facialexpressionchampionship/SharedPreferencesWrapper.kt
+++ app/src/main/java/com/example/facialexpressionchampionship/SharedPreferencesWrapper.kt
@@ -1,13 +1,10 @@
 package com.example.facialexpressionchampionship
 
 import android.content.Context
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class SharedPreferencesWrapper @Inject constructor(
-    @ApplicationContext context: Context
+    context: Context
 ) {
 
     private val HISTORY_KEY = "history_key"
