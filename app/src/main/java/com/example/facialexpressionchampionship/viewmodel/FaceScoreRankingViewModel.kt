6f8efc6d1e30c937155ae23b183e58cab412c9f2
hkuwabara-cq <h.kuwabara@caraquri.com>
2021-09-18 01:52:40 +0900

    継承先を変更

--- app/src/main/java/com/example/facialexpressionchampionship/viewmodel/FaceScoreRankingViewModel.kt
+++ app/src/main/java/com/example/facialexpressionchampionship/viewmodel/FaceScoreRankingViewModel.kt
@@ -3,7 +3,7 @@ package com.example.facialexpressionchampionship.viewmodel
 import androidx.databinding.ObservableField
 import com.example.facialexpressionchampionship.R
 import com.example.facialexpressionchampionship.SharedPreferencesWrapper
-import com.example.facialexpressionchampionship.data.BattleHistoryRepositoryImpl
+import com.example.facialexpressionchampionship.data.BattleHistoryRepository
 import com.example.facialexpressionchampionship.data.room.BattleInformationEntity
 import com.example.facialexpressionchampionship.data.room.ChallengerEntity
 import com.example.facialexpressionchampionship.model.ScoreData
@@ -13,7 +13,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FaceScoreRankingViewModel @Inject constructor(
-    private val battleHistoryRepositoryImpl: BattleHistoryRepositoryImpl,
+    private val repository: BattleHistoryRepository,
     private val sharedPreference: SharedPreferencesWrapper
 ) : BaseViewModel() {
 
@@ -31,8 +31,8 @@ class FaceScoreRankingViewModel @Inject constructor(
         }
 
         val battleInformation = BattleInformationEntity(sharedPreference.getBattleId(), name, battleTheme)
-        battleHistoryRepositoryImpl.saveBattleInformation(battleInformation)
-            .andThen(battleHistoryRepositoryImpl.saveChallenger(createChallengerList(scoreDataList)))
+        repository.saveBattleInformation(battleInformation)
+            .andThen(repository.saveChallenger(createChallengerList(scoreDataList)))
             .execute(
                 onComplete = {
                     sharedPreference.saveBattleId(sharedPreference.getBattleId())
